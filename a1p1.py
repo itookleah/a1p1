16907d879995d1fc964e30878145c5521f4fd237
Leah Vann <lvann@uci.edu>
2024-01-22 20:21:56 -0800

    quit and view command working

--- a1p1.py
+++ a1p1.py
@@ -7,15 +7,17 @@
 from pathlib import Path
 
 def view():
-    myPath = Path("C:\ics32")
+    myPath = Path("\ics32")
     for currentPath in myPath.iterdir():
         print(currentPath)
 
 
 def commands():
     command = input("Please view(L) or quit(Q): \n")
+    while command != 'q':
+        view()
+        command = input("Please view(L) or quit(Q): \n")
 
 
 if __name__ == "__main__":
-    while commands() != 'q':
-        view()
+    commands()
