66635bbb42f5f9063169e31897b4110e6d3b3c1f
Leah Vann <lvann@uci.edu>
2024-01-29 20:00:32 -0800

    adding the create file function

--- a1p1.py
+++ a1p1.py
@@ -10,10 +10,12 @@ command = input("View specifed directory([COMMAND] [INPUT] [[-]OPTION] [INPUT]) 
 
 #View (L) or Quit (Q)
 command1 = ''
-#Input
+#Directory name
 command2 = ''
 #[-]Option
 command3 = ''
+#input
+command4 = ''
 
 dir = Path(command2)
 
@@ -24,14 +26,20 @@ for i, comm in enumerate(command):
             command1 = command[start:i]
         elif not command2:
             command2 = command[start:i]
+        elif not command3:
+            command3 = command[start:i]
+        else:
+            command4 = command[start:i]
         start = i + 1
 if not command2 and start < len(command):
     command2 = command[start:]
 elif not command3 and start < len(command):
     command3 = command[start:]
-
+elif not command4 and start < len(command):
+    command4 = command[start:]
 
 def view_files(directory):
+    '''Views files in specified directory'''
     myPath = Path(directory)
     for currentPath in myPath.iterdir():
         if currentPath.is_file():
@@ -39,29 +47,46 @@ def view_files(directory):
     
 
 def view_directories(directory):
+    '''View folders in specified directory'''
     myPath = Path(directory)
     for currentPath in myPath.iterdir():
         if currentPath.is_dir():
             print(currentPath)
 
 
-def recursive_directories():
-    view_files(dir)
-    for item in dir.iterdir():
+def recursive_directories(directory):
+    view_files(directory)
+    for item in directory.iterdir():
         if item.is_dir():
             print(item)
             view_files(item)
+            recursive_directories(item)
 
+
+def create_file():
+    new = Path(command2 + command4 + '.dsu')
+    new.touch()
+
+
+def delete_file(directory):
+    pass
 
 def commands():
     if command1 == 'L':
         if command3 == '-f':
             view_files(command2)
         elif command3 == '-r':
-            recursive_directories()
+            recursive_directories(command2)
         else:
             view_files(command2)
             view_directories(command2)
+    elif command1 == 'C':
+        if command3 =='-n':
+            create_file()
+    elif command1 == 'D':
+        pass
+    elif command1 == 'R':
+        pass
 
 
 if __name__ == "__main__":
